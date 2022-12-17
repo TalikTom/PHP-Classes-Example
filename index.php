5b4ce7d644dcb1962bb5bb153ccab4b38f0cb1da
Luka Agic <lukagic@gmail.com>
2022-12-17 15:49:31 +0100

    Set Customer class

--- index.php
+++ index.php
@@ -1,4 +1,26 @@
 <?php
+class Customer 
+{
+    public string $firstname;
+    public string $lastname;
+    public string $email;
+    public string $password;
+    public array $accounts;
 
 
+    function __construct(string $firstname, string $lastname, string $email, string $password, array $accounts)
+    {
+        $this->firstname = $firstname;
+        $this->lastname = $lastname;
+        $this->email = $email;
+        $this->password = $password;
+        $this->accounts = $accounts;
+    }
+
+    function getFullName()
+    {
+        return $this->firstname . ' ' . $this->lastname;
+    }
+
+}
 ?>
