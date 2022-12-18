e16a2687d0a9509b2f171644331120b866f55c64
Luka Agic <lukagic@gmail.com>
2022-12-18 12:01:49 +0100

    Add css

--- index.php
+++ index.php
@@ -38,21 +38,21 @@ $customer = new Customer('Luka', 'Agic', 'luka@gmail.com', 'hunter2', $accounts)
         <?php } ?>
 
         $<?= $account->getBalance() ?></td>
-        j
+        
     </tr>
     <?php } ?>
 </table>
-<br>
+
 <table>
-    <tr><th colspan="2">Data about broswer sent in HTTP headers</th></tr>
+    <tr><th colspan="2" class="title">Data about broswer sent in HTTP headers</th></tr>
     <tr><th>Broswer IP address</th><td><?= $_SERVER['REMOTE_ADDR'] ?></td></tr>
     <tr><th>Type of browser</th><td><?= $_SERVER['HTTP_USER_AGENT'] ?></td></tr>
-    <tr><th colspan="2">HTTP request</th></tr>
+    <tr><th colspan="2" class="title">HTTP request</th></tr>
     <tr><th>Host name</th><td><?= $_SERVER['HTTP_HOST'] ?></td></tr>
     <tr><th>URI after host name</th><td><?= $_SERVER['REQUEST_URI'] ?></td></tr>
     <tr><th>Query string</th><td><?= $_SERVER['QUERY_STRING'] ?></td></tr>
     <tr><th>HTTP request method</th><td><?= $_SERVER['REQUEST_METHOD'] ?></td></tr>
-    <tr><th colspan="2">Location of the file being executed</th></tr>
+    <tr><th colspan="2" class="title">Location of the file being executed</th></tr>
     <tr><th>Document root</th><td><?= $_SERVER['DOCUMENT_ROOT'] ?></td></tr>
     <tr><th>PATH from document root</th><td><?= $_SERVER['SCRIPT_NAME'] ?></td></tr>
     <tr><th>Absolute path</th><td><?= $_SERVER['SCRIPT_FILENAME'] ?></td></tr>
