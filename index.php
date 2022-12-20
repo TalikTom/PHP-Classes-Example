b187b5536cd060b9a9f4ca90d4fa687695c9195e
Luka Agic <lukagic@gmail.com>
2022-12-21 00:19:36 +0100

    Practice Post method and refactor code

--- index.php
+++ index.php
@@ -43,7 +43,7 @@ $cities =
         'Vukovar' => 'Osjecka 10, 32000'
     ];
 
-$city = isset($_GET['city']) ? $_GET['city'] : '';
+$city = $_GET['city'] ?? '';
 $address = $cities[$city];
 $valid = array_key_exists($city, $cities);
 if ($valid) {
@@ -204,5 +204,26 @@ echo "The time is " . date("h:i:sa");
 <h4>XSS attack example</h4>
 <a href="?msg=<script src=js/bad.js></script>" class="badlink">Link to demo xss attack</a>
 <p><?= htmlspecialchars($message) ?></p>
+
+<form action="index.php" method="POST">
+  <p>Name:     <input type="text" name="name"></p>
+  <p>Age:      <input type="text" name="age"></p>
+  <p>Email:    <input type="text" name="email"></p>
+  <p>Password: <input type="password" name="pwd"></p>
+  <p>Bio:      <textarea name="bio"></textarea></p>
+  <p>Contact preference:
+    <select name="preferences">
+      <option value="email">Email</option>
+      <option value="phone">Phone</option>
+    </select></p>
+  <p>Rating: 
+    1 <input type="radio" name="rating" value="1">
+    2 <input type="radio" name="rating" value="2">
+    3 <input type="radio" name="rating" value="3"></p>
+  <p><input type="checkbox" name="terms" value="true"> 
+  I agree to the terms and conditions.</p>
+  <p><input type="submit" value="Save"></p>
+</form>
+<pre><?php var_dump($_POST); ?>
 
 <?php include 'includes/footer.php'; ?>
